f40dcd96185b574233d337b5fe1c207d413c1d4d
okpocandy <opcandy@naver.com>
2022-06-06 14:41:41 +0900

    add delete method

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -6,10 +6,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,12 +29,24 @@ public class DummyControllerTest {
 	
 	@Autowired	//의존성 주입(DI)
 	private UserRepository userRepository;
-	
-	
 	//save함수는 id를 전달하지 앟으면 insert를 해주고
 	//save함수는 id를 전달하면 해당 id에 대한 데이터가 잇으면 update를 해주고
 	//save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해준다.
 	// email, password 를 수정
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+		}
+		
+		
+		return "삭제되었습니다. id : "+id;
+	}
+	
+	
 	@Transactional	// 함수 종료시 자동 commit이 됨.
 	@PutMapping("/dummy/user/{id}")
 	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //@Requestbody 로 json데이터 받음.
@@ -50,7 +64,7 @@ public class DummyControllerTest {
 		// userRepository.save(user);	
 		
 		// @Trarnsactional 더티 채킹
-		return null;
+		return user;
 	}
 	
 	
